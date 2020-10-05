f6c5b7c7e2cfa0cda2335300d16f953ea0fe2570
Sam Abbott <s.e.abbott12@gmail.com>
2020-10-05 15:59:16 +0100

    Update dataset-list.R

--- R/dataset-list.R
+++ R/dataset-list.R
@@ -9,17 +9,20 @@ datasets <- c(
   Region$new(name = "united-kingdom",
              covid_regional_data_identifier = "UK",
              reporting_delay = readRDS(here::here("data", name-of-onset-to-admission-delay)),
-             case_modifier = function(cases) { put your cleaning code in here or remove if not needed}),
+             case_modifier = function(cases) { put your cleaning code in here or remove if not needed},
+             stan_args = list(nhs = TRUE)),
   Region$new(name = "united-kingdom",
              covid_regional_data_identifier = "UK",
              dataset_folder_name = "deaths",
              reporting_delay = readRDS(here::here("data", name-of-onset-to-death-delay)),
-             case_modifier = function(cases) { put your cleaning code in here or remove if not needed})
+             case_modifier = function(cases) { put your cleaning code in here or remove if not needed},
+             stan_args = list(nhs = TRUE))
   Region$new(name = "united-kingdom",
              covid_regional_data_identifier = "UK",
              dataset_folder_name = "admissions"
              reporting_delay = readRDS(here::here("data", name-of-onset-to-admission-delay)),
-             case_modifier = function(cases) { put your cleaning code in here or remove if not needed})
+             case_modifier = function(cases) { put your cleaning code in here or remove if not needed},
+             stan_args = list(nhs = TRUE))
   Region$new(name = "united-states",
              covid_regional_data_identifier = "USA",
              region_scale = "State"),

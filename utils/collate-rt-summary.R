cccce8746f4c009844503ae747477cfd1a0b9554
Sam Abbott <s.e.abbott12@gmail.com>
2020-10-06 19:29:59 +0100

    add target, defaults to rt, ...
    moves saving into target folder and makes save file name the date of running the function

--- utils/collate-rt-summary.R
+++ utils/collate-rt-summary.R
@@ -1,25 +1,25 @@
 # Collate estimates from different estimates in same sub-regional folder 
 
-collate_estimates <- function(name){
+collate_estimates <- function(name, target = "rt"){
   
   # Get locations of summary csv
   sources <- as.list(paste0(list.files(here::here("subnational", name), full.names = TRUE),
-                            "/summary/rt.csv"))
+                            "/summary/", target, ".csv"))
   names(sources) <- list.files(here::here("subnational", name))
   
   # Read and bind
-  rt <- purrr::discard(sources, grepl("collated", names(sources))) %>%
+  df <- purrr::discard(sources, grepl("collated", names(sources))) %>%
     purrr::map(., ~ fread(.x)) %>%
     dplyr::bind_rows(.id = "source") %>%
     dplyr::filter(type == "estimate") %>%
     dplyr::select(-type)
   
   # Check a collated file exists
-  if(!dir.exists(here::here("subnational", name, "collated", Sys.Date()))){
-    dir.create(here::here("subnational", name, "collated", Sys.Date()))
+  if(!dir.exists(here::here("subnational", name, "collated", target))){
+    dir.create(here::here("subnational", name, "collated", target))
   }
   
   # Save back to main UK folder
-  data.table::fwrite(rt, here::here("subnational", name, "collated", Sys.Date(), "uk-collated-rt.csv"))
+  data.table::fwrite(df, here::here("subnational", name, "collated", target, paste0(Sys.Date(), ".csv")))
   return(invisible(NULL))
 }

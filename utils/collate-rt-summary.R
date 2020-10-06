1095f02a0779051ad4d9b91a0a974081a5231177
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2020-10-06 18:26:35 +0100

    Collate Rt estimates
    Adds script to collate Rt estimates from different data sources within one region, creating a new folder (`collate/`) with one csv output.
    
    Uses a new dependency, `purrr` - but open to a re-write to avoid this if necessary.
    
    In general obviously happy for this to be edited to better fit with the infrastructure.

---
+++ utils/collate-rt-summary.R
@@ -0,0 +1,25 @@
+# Collate estimates from different estimates in same sub-regional folder 
+
+collate_estimates <- function(name){
+  
+  # Get locations of summary csv
+  sources <- as.list(paste0(list.files(here::here("subnational", name), full.names = TRUE),
+                            "/summary/rt.csv"))
+  names(sources) <- list.files(here::here("subnational", name))
+  
+  # Read and bind
+  rt <- purrr::discard(sources, grepl("collated", names(sources))) %>%
+    purrr::map(., ~ fread(.x)) %>%
+    dplyr::bind_rows(.id = "source") %>%
+    dplyr::filter(type == "estimate") %>%
+    dplyr::select(-type)
+  
+  # Check a collated file exists
+  if(!dir.exists(here::here("subnational", name, "collated", Sys.Date()))){
+    dir.create(here::here("subnational", name, "collated", Sys.Date()))
+  }
+  
+  # Save back to main UK folder
+  write.csv(rt, here::here("subnational", name, "collated", Sys.Date(), "uk-collated-rt.csv"), row.names = FALSE)
+  
+}

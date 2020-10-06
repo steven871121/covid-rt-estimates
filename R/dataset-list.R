592e91110ed067197b2ca6e731a1e24e345035b5
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-10-06 12:26:59 +0100

    _ not - in file name

--- R/dataset-list.R
+++ R/dataset-list.R
@@ -19,7 +19,7 @@ datasets <- c(
              covid_regional_data_identifier = "UK",
              folder_name = "united-kingdom",
              dataset_folder_name = "deaths",
-             reporting_delay = readRDS(here::here("data", "cocin-onset-to-death-delay.rds")),
+             reporting_delay = readRDS(here::here("data", "cocin_onset_to_death_delay.rds")),
              case_modifier = function(deaths) {
                deaths <- deaths[, cases_new := deaths_new]
                national_deaths <- deaths[region %in% c("England", "Scotland", "Wales", "Northern Ireland"),]
